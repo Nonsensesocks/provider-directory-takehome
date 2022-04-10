a6645efd4023a9cd2a727f54386a5bc099945a19
nonsensesocks <champion.crystal@outlook.com>
2022-04-10 13:44:22 -0600

    cleaning up unnecessary function

--- src/pages/BrowseProviders.js
+++ src/pages/BrowseProviders.js
@@ -3,16 +3,11 @@ import Header from '../components/Header'
 import ProvidersList from '../components/ProvidersList'
 
 function BrowseProviders({locations, selectedLocation, localProviders, setSelectedLocation, setSelectedProvider}) {
-  function setLocation(location) {
-    console.log(location)
-    setSelectedLocation(location)
-  }
-
   return (
     <div className="App">
         <div>
             <Header 
-              setSelectedLocation={setLocation}
+              setSelectedLocation={setSelectedLocation}
               location={selectedLocation}
               locations={locations}
             />
