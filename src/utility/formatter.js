7d4cf03b8953e181214f3c45b79a266750bc8872
nonsensesocks <champion.crystal@outlook.com>
2022-04-09 21:18:28 -0600

    formatter - added clear comments adjusted language 'string' to be consistently 'text'

--- src/utility/formatter.js
+++ src/utility/formatter.js
@@ -5,13 +5,11 @@ export function truncateText(text, length) {
         let textToLength = text.substr(0, length);
         // cut unfinished words
         let textNoUnfinishedWords = textToLength.substr(0, Math.min(length, textToLength.lastIndexOf(" ")));
-
         // if last character is punctuation, remove it
         let lastCharReplacePunctuation = (textNoUnfinishedWords.slice(-1)).replace(punctuationRegex, '')
-        
-        let truncatedString = textNoUnfinishedWords.substr(0, (textNoUnfinishedWords.length - 1)) + lastCharReplacePunctuation
-
-        return truncatedString + '...';
+        // truncated text = text with no unfinished words - the last character + the last character passed through regex
+        let truncatedText = textNoUnfinishedWords.substr(0, (textNoUnfinishedWords.length - 1)) + lastCharReplacePunctuation
+        return truncatedText + '...';
     }
     return text
 }
